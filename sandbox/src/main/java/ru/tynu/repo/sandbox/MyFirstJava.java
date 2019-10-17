496e932cb60f4ab142189acc31da7bb4e4fe2a62
tynu4ok <tynu4ok@mail.ru>
2019-10-18 00:49:14 +0300

    fdf

--- sandbox/src/main/java/ru/tynu/repo/sandbox/MyFirstJava.java
+++ sandbox/src/main/java/ru/tynu/repo/sandbox/MyFirstJava.java
@@ -2,7 +2,24 @@ package ru.tynu.repo.sandbox;
 
 public class MyFirstJava {
   public static void main(String[] args) {
-    System.out.println("Hello, world!");
+    hello("user");
+    double len = 5.0;
+    System.out.println("Площадь квадрата со стороной " + len + " =  " + area(len));
+    double a = 4;
+    double b = 6;
+    System.out.println("Прощадь прямоугольника со стороной " + a + " и " + b + " = " + area(a,b));
+  }
+
+  public static void hello(String somebody) {
+      System.out.println("Hello, " + somebody + "!");
+  }
+
+  public static double area(double l){
+    return l * l;
+  }
+
+  public static double area(double a, double b){
+    return a * b;
   }
 }
 
